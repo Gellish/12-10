80f32b20863c4bd8697b3081cd6ff6000aaebe66
Gellish <40665645+Gellish@users.noreply.github.com>
2019-01-06 22:14:01 -0800

    Update coffee.js

--- JavaScript 5/coffee.js
+++ JavaScript 5/coffee.js
@@ -21,27 +21,40 @@ function level1() {
 function level2() {
 	var amount = document.querySelector("#coffee").value;
 	var output = document.querySelector("#answer");
-	if (amount >= 30.00) {
+if (isNaN(amount)) {
+		let error = output.innerHTML = "No letters allow";
+		output.style.color = "red";
+		output.style.fontSize = "25px";
+		alert(error);
+	}
+  else if (amount >= 25.00) {
 		alert("ENJOY YOUR COFEE");
 		output.innerHTML = "ENJOY YOUR COFEE";
 		output.style.color = "green";
 	}
+
 	else {
 		output.innerHTML = "Insuffecient Amount";
-		output.style.color = "red";
+		output.style.color = "orange";
 	}
 }
 function level3() {
 	var amount = document.querySelector("#coffee").value;
 	var output = document.querySelector("#answer");
-	if (amount >= 50.00) {
+if (isNaN(amount)) {
+		let error = output.innerHTML = "No letters allow";
+		output.style.color = "red";
+		output.style.fontSize = "25px";
+		alert(error);
+	}
+  else if (amount >= 25.00) {
 		alert("ENJOY YOUR COFEE");
 		output.innerHTML = "ENJOY YOUR COFEE";
 		output.style.color = "green";
 	}
+
 	else {
 		output.innerHTML = "Insuffecient Amount";
-		output.style.color = "red";
+		output.style.color = "orange";
 	}
 }
-
