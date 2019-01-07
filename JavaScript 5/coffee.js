1e804014640d1c010803fddc4e02e42b55d20243
Gellish <40665645+Gellish@users.noreply.github.com>
2019-01-08 00:39:29 +1200

    Update coffee.js

--- JavaScript 5/coffee.js
+++ JavaScript 5/coffee.js
@@ -1,41 +1,43 @@
-fuction accept_input() {
-  	var input = Number(document.querySelector("#coffee").value);
-	  return input;
-}
- fuction checkamount(amount,price) {
-  	if (isNaN(amount)) {
-		let error = output.innerHTML = "No letters allow";
-		output.style.color = "red";
-		output.style.fontSize = "25px";
-		alert(error);
-	}
-  else if (amount >= price) {
-		alert("ENJOY YOUR COFEE");
-		output.innerHTML = "ENJOY YOUR COFEE";
-		output.style.color = "green";
-	}
+    function accept_input(){
+        var input = Number(document.querySelector("#coffee").value);
+        return input;
+    }
 
-	else {
-		output.innerHTML = "Insuffecient Amount";
-		output.style.color = "orange";
-	}
-} 
+    function condition(amount,price) {
+        var output = document.querySelector("#answer");
+        var balance = document.querySelector("#balance");
+        if (isNaN(amount)) {
+            var error = output.innerHTML = "No letters allow";
+            output.style.color = "red";
+            alert(error);
+        }
+        else if (amount >= price) {
+            alert("ENJOY YOUR COFEE");
+            output.innerHTML = "ENJOY YOUR COFEE";
+            output.style.color = "green";
+            var result = amount-price;
+            balance.innerHTML = result;
+            balance.style.color = "green";
+        }
+        else {
+            output.innerHTML = "Insuffecient Amount";
+            balance.innerHTML = "0";
+            output.style.color = "orange";
+        }
+    }
 
-function level1() {
-  var input = accept_input()
-  var cprice = 25;
-  checkamount(input,price)
-  }
-
-function level2() {
-  var input = accept_input()
-  var price = 30;
-  checkamount(input,price)
-}
-
-function level3() {
- var input = accept_input()
- var cprice = 50;
- checkamount(input,price)
-}
-
+    function level1() {
+        var input = accept_input();
+        var price = 25;
+        condition(input,price);
+    }
+    function level2() {
+        var input = accept_input();
+        var price = 30;
+        condition(input,price);
+    }
+    function level3() {
+        var input = accept_input();
+        var price = 50;
+        condition(input,price);
+    } 
